5e27affb4f60f096f79c27c2794866b427b5a2f2
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-03-13 16:25:24 +0100

    improve test

--- tests/test_extract_function.py
+++ tests/test_extract_function.py
@@ -1,12 +1,34 @@
-from devtools import debug
+import pytest
 from fractal_task_tools._signature_constraints import _extract_function
 
 
 def test_extract_function():
-    fun1 = _extract_function(
+    function = _extract_function(
         module_relative_path="_create_manifest.py",
         package_name="fractal_task_tools",
         function_name="create_manifest",
         verbose=True,
     )
-    debug(fun1)
+    assert function.__name__ == "create_manifest"
+
+    with pytest.raises(
+        AttributeError,
+        match="has no attribute 'missing_function'",
+    ):
+        _extract_function(
+            module_relative_path="_create_manifest.py",
+            package_name="fractal_task_tools",
+            function_name="missing_function",
+            verbose=True,
+        )
+
+    with pytest.raises(
+        ModuleNotFoundError,
+        match="No module named 'fractal_task_tools.missing_module'",
+    ):
+        _extract_function(
+            module_relative_path="missing_module.py",
+            package_name="fractal_task_tools",
+            function_name="missing_function",
+            verbose=True,
+        )
