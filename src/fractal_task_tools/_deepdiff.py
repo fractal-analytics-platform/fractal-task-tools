9d7b3e923d754343c780e8b5cfb3526df738264a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-03-18 15:27:20 +0100

    Fix `type(None)` and max-recursion error

--- src/fractal_task_tools/_deepdiff.py
+++ src/fractal_task_tools/_deepdiff.py
@@ -3,6 +3,8 @@ from typing import Union
 
 ValidType = Union[list, dict, str, int, float, bool, None]
 
+MAX_RECURSION_LEVEL = 20
+
 
 def deepdiff(
     *,
@@ -10,6 +12,7 @@ def deepdiff(
     new_object: ValidType,
     path: str,
     ignore_keys_order: bool,
+    recursion_level: int = 1,
 ):
     if type(old_object) is not type(new_object):
         raise ValueError(
@@ -17,8 +20,11 @@ def deepdiff(
             f"\tOld: {type(old_object)}\n\tNew:{type(new_object)}"
         )
 
-    if type(old_object) not in [list, dict, str, int, float, bool, None]:
+    if type(old_object) not in [list, dict, str, int, float, bool, type(None)]:
         raise ValueError(f"[{path}] Invalid type {type(old_object)}, exit.")
+
+    if recursion_level > MAX_RECURSION_LEVEL:
+        raise ValueError(f"Reached {MAX_RECURSION_LEVEL=}. Exit.")
 
     if type(old_object) is dict:
         old_keys = list(old_object.keys())
@@ -38,6 +44,7 @@ def deepdiff(
                 new_object=new_object[key],
                 path=f"{path}['{key}']",
                 ignore_keys_order=ignore_keys_order,
+                recursion_level=recursion_level + 1,
             )
     elif type(old_object) is list:
         if len(old_object) != len(new_object):
@@ -51,6 +58,7 @@ def deepdiff(
                 new_object=new_object[ind],
                 path=f"{path}[{ind}]",
                 ignore_keys_order=ignore_keys_order,
+                recursion_level=recursion_level + 1,
             )
     else:
         if old_object != new_object:
