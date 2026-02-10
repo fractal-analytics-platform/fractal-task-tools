dc046ba768d0d5111f5d6ae2bec59f1f12bc68df
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-02-10 09:56:20 +0100

    refactor

--- src/fractal_task_tools/logging_config.py
+++ src/fractal_task_tools/logging_config.py
@@ -1,57 +1,57 @@
 import logging
 import os
-from typing import Literal
+import typing
 
-ALLOWED_LOGGING_LEVELS = (
-    "CRITICAL",
-    "ERROR",
-    "WARNING",
-    "INFO",
+
+ValidLoggingLevel = typing.Literal[
     "DEBUG",
-)
-DEFAULT_LOG_LEVEL = "INFO"
-DEFAULT_LOG_FORMAT = r"%(asctime)s; %(name)s; %(levelname)s; %(message)s"
+    "INFO",
+    "WARNING",
+    "ERROR",
+    "CRITICAL",
+]
+ALLOWED_LOGGING_LEVELS: tuple[str, ...] = typing.get_args(ValidLoggingLevel)
+
+
+DEFAULT_LOG_LEVEL: ValidLoggingLevel = "INFO"
+DEFAULT_LOG_FORMAT: str = r"%(asctime)s; %(name)s; %(levelname)s; %(message)s"
 
 
 def get_logging_format() -> str:
-    # Use default values if env variables are unset or set to an empty string
-    log_format = os.getenv("FRACTAL_TASK_LOG_FORMAT", None)
-    log_format = log_format or DEFAULT_LOG_FORMAT
+    """
+    Get valid logging format from environment variable or default value.
+    """
+    # Use default value if the env variable is unset or set to an empty string
+    log_format = os.getenv("FRACTAL_TASK_LOG_FORMAT") or DEFAULT_LOG_FORMAT
+    # Validate `log_format`
     logging.PercentStyle(fmt=log_format).validate()
     return log_format
 
 
-def get_logging_level() -> (
-    Literal[
-        "CRITICAL",
-        "ERROR",
-        "WARNING",
-        "INFO",
-        "DEBUG",
-    ]
-):
-    # Get env variables
-    FRACTAL_TASK_LOG_LEVEL = os.getenv(
-        "FRACTAL_TASK_LOG_LEVEL",
-        None,
-    )
-    # Use default values if env variables are unset or set to an empty string
-    FRACTAL_TASK_LOG_LEVEL = FRACTAL_TASK_LOG_LEVEL or DEFAULT_LOG_LEVEL
-
-    # Validate log-level variable
-    if FRACTAL_TASK_LOG_LEVEL not in ALLOWED_LOGGING_LEVELS:
+def get_logging_level() -> ValidLoggingLevel:
+    """
+    Get valid logging level from environment variable or default value.
+    """
+    # Use default value if the env variable is unset or set to an empty string
+    log_level = os.getenv("FRACTAL_TASK_LOG_LEVEL") or DEFAULT_LOG_LEVEL
+    # Validate `log_level`
+    if log_level not in ALLOWED_LOGGING_LEVELS:
         raise ValueError(
-            f"Invalid {FRACTAL_TASK_LOG_LEVEL=} environment variables. "
-            f"Allowed values: {ALLOWED_LOGGING_LEVELS}."
+            f"Invalid FRACTAL_TASK_LOG_LEVEL={log_level} environment "
+            f"variable. Allowed values: {ALLOWED_LOGGING_LEVELS}."
         )
+    return log_level
 
-    return FRACTAL_TASK_LOG_LEVEL
 
+def setup_logging_config() -> None:
+    """
+    Configure root logging handler.
 
-def setup_logging_config():
+    Note that calling `logging.basicConfig` with `force=True` removes all
+    existing handlers of the `root` logger and creates a new handler.
+    """
     FRACTAL_TASK_LOG_LEVEL = get_logging_level()
     FRACTAL_TASK_LOG_FORMAT = get_logging_format()
-    # Apply logging configuration
     logging.basicConfig(
         level=FRACTAL_TASK_LOG_LEVEL,
         format=FRACTAL_TASK_LOG_FORMAT,
