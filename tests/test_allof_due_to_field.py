09aaa72cdf282144923311d49622e48e3ae55335
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-02-12 16:40:10 +0100

    update test

--- tests/test_allof_due_to_field.py
+++ tests/test_allof_due_to_field.py
@@ -1,29 +1,28 @@
 from devtools import debug
 from fractal_task_tools._args_schemas import create_schema_for_single_task
-from pydantic import validate_call, Field, BaseModel
+from pydantic import validate_call
+from enum import Enum
 
 
-class MyModel(BaseModel):
-    x: int
-    y: int
+class MyEnum(Enum):
+    OPTION1 = "option1"
 
 
 @validate_call
-def task_function_with_field(
-    zarr_url: str,
-    arg_1: MyModel,
-    arg_2: MyModel = Field(default_factory=MyModel),
-):
+def task_function_with_field(arg_1: MyEnum = MyEnum.OPTION1):
     """
     Short description
 
     Args:
         arg_1: Description of arg_1.
-        arg_2: Description of arg_2.
     """
 
 
 def test_create_schema_with_field():
+    """
+    When running with pydantic 2.8.2, this test verifies that
+    `_remove_top_level_single_element_allof` is effective.
+    """
     schema = create_schema_for_single_task(
         task_function=task_function_with_field,
         executable=__file__,
@@ -31,23 +30,12 @@ def test_create_schema_with_field():
         verbose=True,
     )
     debug(schema)
-    # target_schema = {
-    #     "additionalProperties": False,
-    #     "properties": {
-    #         "zarr_url": {
-    #             "title": "Zarr Url",
-    #             "type": "string",
-    #             "description": "Missing description",
-    #         },
-    #         "arg_1": {
-    #             "default": 1,
-    #             "title": "Arg 1",
-    #             "type": "integer",
-    #             "description": "Description of arg_1.",
-    #         },
-    #     },
-    #     "required": ["zarr_url"],
-    #     "type": "object",
-    #     "title": "TaskFunction",
-    # }
-    # assert target_schema == schema
+    target_properties = {
+        "arg_1": {
+            "$ref": "#/$defs/MyEnum",
+            "default": "option1",
+            "title": "Arg 1",
+            "description": "Description of arg_1.",
+        },
+    }
+    assert target_properties == schema["properties"]
