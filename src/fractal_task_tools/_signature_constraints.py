5c921a278dfa60265052329c87f462641f7cec4a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-02-18 16:23:45 +0100

    Add debugging

--- src/fractal_task_tools/_signature_constraints.py
+++ src/fractal_task_tools/_signature_constraints.py
@@ -189,6 +189,8 @@ def _recursive_union_validation(
 
     if type(annotation) is type(BaseModel):
         for attribute_name, field_info in annotation.model_fields.items():
+            from devtools import debug
+            debug(field_info, annotation.__annotations__)
             _recursive_union_validation(
                 name=f"{name}['{attribute_name}']",
                 annotation=annotation.__annotations__[attribute_name],
