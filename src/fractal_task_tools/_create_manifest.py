2c71a8a9fab2a5d5c99cdbc06040bb19058a4eb2
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-02-25 09:18:16 +0100

    logging

--- src/fractal_task_tools/_create_manifest.py
+++ src/fractal_task_tools/_create_manifest.py
@@ -21,6 +21,9 @@ MANIFEST_FILENAME = "__FRACTAL_MANIFEST__.json"
 MANIFEST_VERSION = "2"
 
 
+logger = logging.getLogger(__name__)
+
+
 def _get_package_name_from_pyproject(pyproject_path: Path) -> str:
     """
     Infer the package name from a local `pyproject.toml`.
@@ -42,9 +45,13 @@ def _get_package_name_from_pyproject(pyproject_path: Path) -> str:
             pyproject = tomllib.load(f)
         project_table = pyproject["project"]
         if "import-names" in project_table and len(project_table["import-names"]) == 1:
-            return project_table["import-names"][0]
+            output = project_table["import-names"][0]
+            logger.info(f"Identified package name '{output}' from pyproject.toml")
+            return output
         else:
-            return project_table["name"]
+            output = project_table["name"]
+            logger.info(f"Identified package name '{output}' from pyproject.toml")
+            return output
     except Exception as e:
         sys.exit(
             "`--package` was not provided, and discovery based on "
@@ -84,7 +91,7 @@ def create_manifest(
     # Normalize package name
     package_name = normalize_package_name(raw_package_name)
 
-    logging.info(f"Start generating a new manifest for {package_name}")
+    logger.info(f"Start generating a new manifest for {package_name}")
 
     # Prepare an empty manifest
     manifest = dict(
@@ -106,7 +113,7 @@ def create_manifest(
         INPUT_MODELS = getattr(task_list_module, "INPUT_MODELS")
     except AttributeError:
         INPUT_MODELS = []
-        logging.warning(
+        logger.warning(
             "No `INPUT_MODELS` found in task_list module. Setting it to `[]`."
         )
 
@@ -114,7 +121,7 @@ def create_manifest(
     try:
         manifest["authors"] = getattr(task_list_module, "AUTHORS")
     except AttributeError:
-        logging.warning("No `AUTHORS` found in task_list module.")
+        logger.warning("No `AUTHORS` found in task_list module.")
 
     # Load DOCS_LINK
     try:
@@ -122,10 +129,10 @@ def create_manifest(
         # Transform empty string into None
         if DOCS_LINK == "":
             DOCS_LINK = None
-            logging.warning("`DOCS_LINK=` transformed into `DOCS_LINK=None`.")
+            logger.warning("`DOCS_LINK=` transformed into `DOCS_LINK=None`.")
     except AttributeError:
         DOCS_LINK = None
-        logging.warning("No `DOCS_LINK` found in task_list module.")
+        logger.warning("No `DOCS_LINK` found in task_list module.")
 
     # Loop over TASK_LIST, and append the proper task dictionaries
     # to manifest["task_list"]
@@ -156,7 +163,7 @@ def create_manifest(
         for kind in ["non_parallel", "parallel"]:
             executable = task_dict.get(f"executable_{kind}")
             if executable is not None:
-                logging.info(f"[{executable}] START")
+                logger.info(f"[{executable}] START")
                 schema = create_schema_for_single_task(
                     executable,
                     package=package_name,
@@ -169,7 +176,7 @@ def create_manifest(
                     executable_kind=kind,
                 )
 
-                logging.info(f"[{executable}] END (new schema)")
+                logger.info(f"[{executable}] END (new schema)")
                 task_dict[f"args_schema_{kind}"] = schema
 
         # Compute and set `docs_info`
